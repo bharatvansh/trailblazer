193b836ecb86ebfb779f6fcf5635a30d4d76730e
Ayush <ayusin439@gmail.com>
2025-09-29 16:05:21 +0530

    refactor: Remove obsolete settings and client initialization classes

--- trailblazer-fabric/SettingsScreen.java
+++
@@ -1,8 +0,0 @@
-package com.example.trailblazersettings;
-
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
-
-public class SettingsScreen extends Screen {
-    private final RenderSettingsManager renderSettingsManager;
-    private final Screen parent;
